0495ce882a172f925f87e882e60ba42bfe78a195
Thiago Talma <thiago@thiagomt.com>
2024-01-05 14:55:22 -0300

    fzf

--- custom/plugins/fzf/fzf.plugin.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-# CTRL-/ to toggle small preview window to see the full command
-export FZF_CTRL_R_OPTS="
-  --preview 'echo {}' --preview-window up:3:hidden:wrap
-  --bind 'ctrl-/:toggle-preview'
-  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
-  --color header:italic
-  --header 'Press CTRL-Y to copy command into clipboard'"
